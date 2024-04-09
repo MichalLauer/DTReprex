b2d5a00e3eb524be3f0c72a2f078fd0e20a97658
Michal Lauer <michal.lauer.25@gmail.com>
2024-04-09 08:04:11 +0200

    Dependencies

--- test.R
+++ test.R
@@ -1,6 +1,4 @@
-# Run: devtools::install()
-# restart R session
-library(shinytest2)
+if (!"DTReprex" %in% installed.packages()) devtools::install()
 Sys.setenv("CHROMOTE_CHROME" = r"(C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe)")
 app <- DTReprex::app()
-driver <- AppDriver$new(app)
+driver <- shinytest2::AppDriver$new(app)
